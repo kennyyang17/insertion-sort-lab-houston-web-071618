c3dbae6a69d974a0566a3916401769c6296d2308
Kenny Yang <kennyyang19@gmail.com>
2019-07-12 16:33:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,45 @@
+// function findMinAndRemove(array){
+//   let minIndex = 0;
+//   let min = array[0];
+//   for (let i=0;i<array.length;i++) {
+//     if (array[i]< min) {
+//       min = array[i];
+//       minIndex = i;
+//     }
+//     array.splice(minIndex,1);
+//     return min;
+//   }
+// }
+
+// function insertionSort(array){
+//   let result = [];
+//   let minVal;
+//   while (array.length !== 0) {
+//     minVal = findMinAndRemove(array);
+//     result.push(minVal);
+//   }
+//   return result;
+// }
+
 function findMinAndRemove(array){
-  let minIndex = 0;
-  let min = array[0];
-  for (let i=0;i<array.length;i++) {
-    if (array[i]< min) {
-      min = array[i];
-      minIndex = i;
+  let currentMin = array[0]
+  let minIndex = 0
+  for(let i = 0; i < array.length; i++){
+    if(array[i] < currentMin){
+      currentMin = array[i]
+      minIndex = i
     }
-    array.splice(minIndex,1);
-    return min;
   }
+  array.splice(minIndex, 1);
+  return currentMin;
 }
 
 function insertionSort(array){
-  let result = [];
-  let minVal;
-  while (array.length !== 0) {
-    minVal = findMinAndRemove(array);
-    result.push(minVal);
+  let sorted = []
+  let min;
+  while(array.length != 0){
+    min = findMinAndRemove(array)
+    sorted.push(min)
   }
-  return result;
+  return sorted;
 }
-
