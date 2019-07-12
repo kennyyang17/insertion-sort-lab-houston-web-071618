f911eed20d5d24baaa172ff87b20cf17e376e974
Kenny Yang <kennyyang19@gmail.com>
2019-07-12 16:37:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 function findMinAndRemove(array){
-
+  let minIndex = 0;
+  let min = array[0];
+  for (let i=0;i<array.length;i++) {
+    if (array[i]< min) {
+      min = array[i];
+      minIndex = i;
+    }
+  }
+      array.splice(minIndex,1);
+    return min;
 }
 
 function insertionSort(array){
-
+  let result = [];
+  let minVal;
+  while (array.length !== 0) {
+    minVal = findMinAndRemove(array);
+    result.push(minVal);
+  }
+  return result;
 }
+
+
