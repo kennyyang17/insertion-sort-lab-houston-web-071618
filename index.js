c190497425b3757dafa3e3476eafc9af81cbf14d
Kenny Yang <kennyyang19@gmail.com>
2019-07-12 16:35:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-// function findMinAndRemove(array){
-//   let minIndex = 0;
-//   let min = array[0];
-//   for (let i=0;i<array.length;i++) {
-//     if (array[i]< min) {
-//       min = array[i];
-//       minIndex = i;
-//     }
-//     array.splice(minIndex,1);
-//     return min;
-//   }
-// }
+function findMinAndRemove(array){
+  let minIndex = 0;
+  let min = array[0];
+  for (let i=0;i<array.length;i++) {
+    if (array[i]< min) {
+      min = array[i];
+      minIndex = i;
+    }
+    array.splice(minIndex,1);
+    return min;
+  }
+}
 
-// function insertionSort(array){
-//   let result = [];
-//   let minVal;
-//   while (array.length !== 0) {
-//     minVal = findMinAndRemove(array);
-//     result.push(minVal);
-//   }
-//   return result;
-// }
+function insertionSort(array){
+  let result = [];
+  let minVal;
+  while (array.length !== 0) {
+    minVal = findMinAndRemove(array);
+    result.push(minVal);
+  }
+  return result;
+}
 
 function findMinAndRemove(array){
   let currentMin = array[0]
